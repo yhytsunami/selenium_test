b1ffd8f779b195d53ccdf7980ad773ba69985787
yuhaiyang <578957514@qq.com>
2019-05-01 12:48:10 +0800

    commit test

---
+++ src/main/java/MySeleniumTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA
+ * Created By Kinsey
+ * Date: 2019/5/1
+ * Time: 9:40
+ */
+public class MySeleniumTest {
+    public static void main (String[] args){
+    }
+}

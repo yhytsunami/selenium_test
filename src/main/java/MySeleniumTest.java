31cb8df9ac7586f065cb10f30e3cc2bdf96a27c9
yuhaiyang <578957514@qq.com>
2019-05-02 09:03:40 +0800

    selenium 基本启动和访问 注意：驱动要和自己的chrome浏览器匹配，selenium会通过驱动调用浏览器执行访问的过程。

--- src/main/java/MySeleniumTest.java
+++ src/main/java/MySeleniumTest.java
@@ -1,3 +1,7 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Kinsey
@@ -5,6 +9,13 @@
  * Time: 9:40
  */
 public class MySeleniumTest {
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException {
+        //驱动文件路径
+        String path = "E:\\ideaSDK\\selenium\\src\\main\\resources\\chromedriver.exe";
+        //
+        System.setProperty("webdriver.chrome.driver",path);
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.xvideos.com");
+        Thread.sleep(10000L);
     }
 }
